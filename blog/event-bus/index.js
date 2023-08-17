fb0d1265e8feba620fd4c615454967b05d09ac4d
sourabhdeshmukh <sourabh.deshmukh.988@gmail.com>
2023-08-17 13:30:39 -0500

    Updating localhost urls in the services with the kubernetes service urls.

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -12,16 +12,16 @@ app.post("/events", (req, res) => {
 
   events.push(event);
 
-  axios.post("http://localhost:4000/events", event).catch((err) => {
+  axios.post("http://posts-clusterip-svc:4000/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4001/events", event).catch((err) => {
+  axios.post("http://comments-clusterip-svc:4001/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4002/events", event).catch((err) => {
+  axios.post("http://query-clusterip-svc:4002/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4003/events", event).catch((err) => {
+  axios.post("http://moderation-clusterip-svc:4003/events", event).catch((err) => {
     console.log(err.message);
   });
   res.send({ status: "OK" });
